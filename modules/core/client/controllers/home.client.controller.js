8b3220423084bfd6701757806edcb0b2c9f0601d
S00153057 <S00153057@mail.itsligo.ie>
2017-01-26 22:48:09 +0000

    Added icons

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -10,5 +10,31 @@
   function HomeController($scope) {
     var vm = this;
     
+    $scope.icons = [
+      {
+        icon: 'Calendar',
+        url: '/modules/core/client/img/icons/calendar.svg'
+      },
+      {
+        icon: 'Contact',
+        url: '/modules/core/client/img/icons/contact.svg'
+      },
+      {
+        icon: 'Email',
+        url: '/modules/core/client/img/icons/email.svg'
+      },
+      {
+        icon: 'College Map',
+        url: '/modules/core/client/img/icons/MapIcon.svg'
+      },
+      {
+        icon: 'Services',
+        url: '/modules/core/client/img/icons/services.svg'
+      },
+      {
+        icon: 'Timetable',
+        url: '/modules/core/client/img/icons/TimeTable.svg'
+      }
+    ]
   }
 }());
