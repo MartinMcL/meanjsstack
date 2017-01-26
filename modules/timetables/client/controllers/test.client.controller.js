fded39577b9b3d345991d7d964e6eeeef1d3dba3
S00153057 <S00153057@mail.itsligo.ie>
2017-01-26 19:44:33 +0000

    Added factory to test.client.controller.js

--- modules/timetables/client/controllers/test.client.controller.js
+++ modules/timetables/client/controllers/test.client.controller.js
@@ -3,10 +3,36 @@
 
   angular
     .module('timetables')
-    .controller('testController', testController);
+    .controller('testController', testController)
+    .factory('Timetables', Timetables);
 
   testController.$inject = ['$scope'];
 
   function testController($scope) {
+  }
+  function Timetables(){
+        return [
+      {
+        TTDId: 1,
+        TTDName: 'Small coffee breaks',
+        TTDMonday: '111101111011111110111111',
+        TTDTuesday: '111101111011111110111111',
+        TTDWednesday: '111101111011111110111111',
+        TTDThursday: '111101111011111110111111',
+        TTDFriday: '111101111011111110111111',
+        TTDSaturday: '111101111011111110111111',
+        TTDSunday: '111101111011111110111111'
+      }, {
+        TTDId: 2,
+        TTDName: 'Larger coffee breaks',
+        TTDMonday: '111100000011111110111111',
+        TTDTuesday: '111100000011111110111111',
+        TTDWednesday: '111100000011111110111111',
+        TTDThursday: '111100000011111110111111',
+        TTDFriday: '111100000011111110111111',
+        TTDSaturday: '111100000011111110111111',
+        TTDSunday: '111100000011111110111111'
+      },
+    ];
   }
 }());
