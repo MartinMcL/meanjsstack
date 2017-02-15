b7e9f6e192bf2f722f8633c54d7902c323a05bf3
Martin McLaughlin <s00151834@mail.itsligo.ie>
2017-02-15 23:22:08 +0000

    Made changes to the User model, to restrict accounts to use s00123456 format, and created an array for user college events

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -39,8 +39,8 @@ var validateLocalStrategyEmail = function (email) {
  * - not begin or end with "."
  */
 
-var validateUsername = function(username) {
-  var usernameRegex = /^(?=[\w.-]+$)(?!.*[._-]{2})(?!\.)(?!.*\.$).{3,34}$/;
+var validateUsername = function (username) {
+  var usernameRegex = /^[sS][0-9]{8}$/;
   return (
     this.provider !== 'local' ||
     (username && usernameRegex.test(username) && config.illegalUsernames.indexOf(username) < 0)
@@ -82,7 +82,7 @@ var UserSchema = new Schema({
     type: String,
     unique: 'Username already exists',
     required: 'Please fill in a username',
-    validate: [validateUsername, 'Please enter a valid username: 3+ characters long, non restricted word, characters "_-.", no consecutive dots, does not begin or end with dots, letters a-z and numbers 0-9.'],
+    validate: [validateUsername, 'Please enter a valid student username in the form - S00123456'],
     lowercase: true,
     trim: true
   },
@@ -124,7 +124,16 @@ var UserSchema = new Schema({
   },
   resetPasswordExpires: {
     type: Date
-  }
+  },
+  calendarEvents: [{
+    title: String,
+    startsAt: Date,
+    endsAt: Date,
+    color: {
+      primary: String,
+      secondary: String
+    }
+  }]
 });
 
 /**
