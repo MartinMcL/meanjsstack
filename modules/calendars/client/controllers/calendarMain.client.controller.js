c4141f5fa70ebb2bd5de1ff1578d8242e7526297
Martin McLaughlin <s00151834@mail.itsligo.ie>
2017-02-15 01:04:31 +0000

    Added click actions for days and events

--- modules/calendars/client/controllers/calendarMain.client.controller.js
+++ modules/calendars/client/controllers/calendarMain.client.controller.js
@@ -11,9 +11,10 @@
     // Add information for the calendar to render
     $scope.calendarView = 'month';
     $scope.viewDate = moment();
+    $scope.delActive = false;
     var result = CalendarFactory.getEvents().then(function (response) {
       $scope.events = response.data[0].events;
-      $scope.events.forEach(function(element) {
+      $scope.events.forEach(function (element) {
         element.startsAt = new Date(element.startsAt);
         element.endsAt = new Date(element.endsAt);
       }, this);
@@ -30,18 +31,51 @@
       'colour': ''
     };
 
+    $scope.eventClicked = function (calendarEvent) {
+      console.log($scope.addEventForm);
+      $scope.addEventForm.calTitle = calendarEvent.title.toString();
+      $scope.addEventForm.startsAtD = calendarEvent.startsAt.getDate();
+      $scope.addEventForm.startsAtM = calendarEvent.startsAt.getMonth();
+      $scope.addEventForm.startsAtY = calendarEvent.startsAt.getFullYear();
+      if (calendarEvent.endsAt != null) {
+        $scope.addEventForm.endsAtD = calendarEvent.endsAt.getDate();
+        $scope.addEventForm.endsAtM = calendarEvent.endsAt.getMonth();
+        $scope.addEventForm.endsAtY = calendarEvent.endsAt.getFullYear();
+      }
+      $scope.addEventForm.colour = calendarEvent.color.primary;
+      console.log(calendarEvent);
+      if (calendarEvent.cssClass === 'college-events') {
+        $scope.delActive = false;
+      }
+      else {
+        $scope.delActive = true;
+      }
+    };
+    $scope.tsClicked = function (calendarDate) {
+      $scope.addEventForm.calTitle = null;
+      $scope.addEventForm.startsAtD = calendarDate.getDate();
+      $scope.addEventForm.startsAtM = calendarDate.getMonth() + 1;
+      $scope.addEventForm.startsAtY = calendarDate.getFullYear();
+      $scope.addEventForm.endsAtD = '';
+      $scope.addEventForm.endsAtM = '';
+      $scope.addEventForm.endsAtY = '';
+      $scope.addEventForm.colour = '';
+    };
     $scope.addEvent = function () {
+      var endevent = new Date($scope.addEventForm.startsAtY, $scope.addEventForm.startsAtM - 1, $scope.addEventForm.startsAtD);
+      if (($scope.addEventForm.endsAtY + $scope.addEventForm.endsAtM + $scope.addEventForm.endsAtD).length > 0) {
+        endevent = new Date($scope.addEventForm.endsAtY, $scope.addEventForm.endsAtM - 1, $scope.addEventForm.endsAtD);
+      }
       var newEvent = {
         title: $scope.addEventForm.calTitle,
         startsAt: new Date($scope.addEventForm.startsAtY, $scope.addEventForm.startsAtM - 1, $scope.addEventForm.startsAtD),
-        endsAt: new Date($scope.addEventForm.endsAtY, $scope.addEventForm.endsAtM - 1, $scope.addEventForm.endsAtD),
+        endsAt: endevent,
         color: {
           primary: $scope.addEventForm.colour,
           secondary: '#fdf1ba'
         }
       };
       $scope.events.push(newEvent);
-      $scope.addEventForm = '';
     };
 
     $scope.prevArrow = function () {
