f6f05899b4dc827952695e67c8f21f1c40d0b604
S00153057 <S00153057@mail.itsligo.ie>
2017-03-01 23:30:05 +0000

    changed icons

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -22,35 +22,44 @@
 
 
     $scope.icons = [{
-      icon: 'Calendar',
-      url: '/modules/core/client/img/icons/calendar.svg',
-      state: '/calendars'
-    },
-    {
-      icon: 'Contact',
-      url: '/modules/core/client/img/icons/contact.svg',
-      state: '/contacts'
-    },
-    {
-      icon: 'Email',
-      url: '/modules/core/client/img/icons/email.svg',
-      state: '/emails'
-    },
-    {
-      icon: 'College Map',
-      url: '/modules/core/client/img/icons/MapIcon.svg',
-      state: '/collegemaps'
-    },
-    {
-      icon: 'Services',
-      url: '/modules/core/client/img/icons/services.svg',
-      state: '/services'
-    },
-    {
-      icon: 'Timetable',
-      url: '/modules/core/client/img/icons/TimeTable.svg',
-      state: '/timetables'
-    }
+        name: 'Timetable',
+        icon: 'glyphicon glyphicon-th',
+        url: '/modules/core/client/img/icons/TimeTable.svg',
+        state: '/timetables'
+      },
+      {
+        name: 'Emails',
+        icon: 'glyphicon glyphicon-envelope',
+        url: '/modules/core/client/img/icons/email.svg',
+        state: '/emails'
+      },
+      {
+        name: 'Calendar',
+        icon: 'glyphicon glyphicon-calendar',
+        url: '/modules/core/client/img/icons/calendar.svg',
+        state: '/calendars'
+      },
+      {
+        name: 'Map',
+        icon: 'glyphicon glyphicon-map-marker',
+        url: '/modules/core/client/img/icons/MapIcon.svg',
+        state: '/collegemaps'
+      },
+      {
+        name: 'Contacts',
+        icon: 'glyphicon glyphicon-earphone',
+        url: '/modules/core/client/img/icons/contact.svg',
+        state: '/contacts'
+      },
+
+
+      {
+        name: 'Services',
+        icon: 'glyphicon glyphicon-th-list',
+        url: '/modules/core/client/img/icons/services.svg',
+        state: '/services'
+      }
+
     ];
   }
 }());
